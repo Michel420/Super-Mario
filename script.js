cdbddeac6d9b4b02e0fb4a03a44e56ff04957f9e
michel420 <micheldiego1514@gmail.com>
2024-09-14 11:40:51 -0300

    Projeto bem simples , esplorando tecnologias js

---
+++ script.js
@@ -0,0 +1,10 @@
+let formulario = document.querySelector(".fale-conosco")
+ console.log(formulario)
+
+function cliqueiNoBotao(){
+   formulario.style.left = "700px"
+}
+
+function sumirFormulario(){
+    formulario.style.left = "-365px"
+}
